1aab8e01dd4fcaab34cf02c2587c7a76a176100b
Roger Winright <32200885+Venpresath@users.noreply.github.com>
2018-07-18 20:20:46 -0400

    Update productService.js

--- productService.js
+++ productService.js
@@ -6,22 +6,22 @@
             name: "World's Best Workbench Mk.I",
             price: 70,
             quantity: 3,
-            image: "/unnamed.jpg"
+            image: "unnamed.jpg"
         },{
             name: "World's Best Workbench Mk.II",
             price: 90,
             quantity: 1,
-            image: "/unnamed.jpg"
+            image: "unnamed.jpg"
         },{
             name: "World's Best Workbench Mk.III",
             price: 290,
             quantity: 1,
-            image: "/unnamed.jpg"
+            image: "unnamed.jpg"
         },{
             name: "World's Best Workbench Mk.IV",
             price: 700,
             quantity: 30,
-            image: "/unnamed.jpg"
+            image: "unnamed.jpg"
         }];
 
         const getProduct = function () {
