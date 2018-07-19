6982d9f34d6f47a9167362a450eecab87776702f
Roger Winright <venpresath@live.com>
2018-07-19 02:33:20 -0400

    added featured items and home page placeholder styling

--- home.js
+++ home.js
@@ -1,11 +1,32 @@
 "use strict"
 {
     let home = {
-        template: `<div><h1>World's Best Workbench</h1></div>`,
-        controller: function () {
+        template: `
+        <div class="homePage">
+            <h1>World's Best Workbench</h1>
+            <h2>Featured Items</h2>
+            <div class="featuredItems">
+            
+                <div class="itemCard">
+                    <h3>{{$ctrl.product[0].name}}</h3>
+                    <img src="{{$ctrl.product[0].image}}" />
+                </div>
+                <div class="itemCard">
+                    <h3>{{$ctrl.product[1].name}}</h3>
+                    <img src="{{$ctrl.product[1].image}}" />
+                </div>
+                <div class="itemCard">
+                    <h3>{{$ctrl.product[3].name}}</h3>
+                    <img src="{{$ctrl.product[3].image}}" />
+                </div>
+            </div>
+        </div>`,
+        controller: function (productService) {
             let vm = this;
+            vm.product = productService.getProduct();
         }
     }
+    home.$inject = ['productService'];
     angular
         .module("app")
         .component("home", home);
--- productService.js
+++ productService.js
@@ -6,27 +6,27 @@
             name: "World's Best Workbench Mk.I",
             price: 70,
             quantity: 3,
-            image: "unnamed.jpg",
-            description: "this one is great"
+            description: "this one is great",
+            image: "unnamed.jpg"
 
         },{
             name: "World's Best Workbench Mk.II",
             price: 90,
             quantity: 1,
-            image: "unnamed.jpg",
-            description: "this one is good too"
+            description: "this one is good too",
+            image: "unnamed.jpg"
         },{
             name: "World's Best Workbench Mk.III",
             price: 290,
             quantity: 1,
-            image: "unnamed.jpg",
-            description: "this one is really awesome"
+            description: "this one is really awesome",
+            image: "unnamed.jpg"
         },{
             name: "World's Best Workbench Mk.IV",
             price: 700,
             quantity: 30,
-            image: "unnamed.jpg",
-            description: "this one is the best one we have"
+            description: "this one is the best one we have",
+            image: "unnamed.jpg"
         }];
 
         const getProduct = function () {
