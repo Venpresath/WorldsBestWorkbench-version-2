ece59e6805bce1bedaac6ee3f28db00221dfe6f8
Roger Winright <venpresath@live.com>
2018-07-25 05:56:15 -0400

    updated a little styling

--- home.js
+++ home.js
@@ -4,22 +4,9 @@
         template: `
         <div class="homePage">
             <h1>World's Best Workbench</h1>
-            <h2>Featured Items</h2>
-            <div class="featuredItems">
-            
-                <div class="itemCard">
-                    <h3>{{$ctrl.product[0].name}}</h3>
-                    <img src="{{$ctrl.product[0].image}}" />
-                </div>
-                <div class="itemCard">
-                    <h3>{{$ctrl.product[1].name}}</h3>
-                    <img src="{{$ctrl.product[1].image}}" />
-                </div>
-                <div class="itemCard">
-                    <h3>{{$ctrl.product[3].name}}</h3>
-                    <img src="{{$ctrl.product[3].image}}" />
-                </div>
-            </div>
+        </div>
+        <div class="offered">
+            <h2>What We Offer</h2>
         </div>`,
         controller: function (productService) {
             let vm = this;
